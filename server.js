2a2f58ac6a84d966062182b83faf2c047a5b2511
Kauan Mattos <kauanosmattos@gmail.com>
2025-04-24 08:31:30 -0300

    Atualização de Comentários

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-document.getElementById('adicionar').addEventListener('click', function () {
-    let inputTarefa = document.getElementById('adicionarTarefa');
+document.getElementById('adicionar').addEventListener('click', function () { // Obtém o elemento de input onde o usuário digita a tarefa
+    let inputTarefa = document.getElementById('adicionarTarefa');  // Obtém o valor digitado, removendo espaços em branco no início e no final
     let textoTarefa = inputTarefa.value.trim(); //espaçamento esquerda
 
     if (textoTarefa === "") return; //caso input vazio
 
-    let lista = document.getElementById('listaTarefa');
+    let lista = document.getElementById('listaTarefa'); // Obtém a lista não ordenada onde as tarefas serão adicionadas
+
+    // Cria um novo item de lista
 
     let item = document.createElement('li')
     item.textContent = textoTarefa;
+
+    // Botão Concluir
 
     let btnConcluido = document.createElement('button');
     btnConcluido.textContent = "Concluir";
     btnConcluido.addEventListener('click', function () {
         item.classList.toggle("concluido");
     })
+
+    // Cria o botão "Remover"
 
     let btnRemover = document.createElement('button');
     btnRemover.textContent = "Remover";
